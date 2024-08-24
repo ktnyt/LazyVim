a8a1baac86fd6a268a29d6c229ee7c708f55c91c
ktnyt <mirei.itaya@lapras.com>
2024-08-25 08:27:53 +0900

    Add search keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,6 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+
+vim.keymap.set("n", "n", "nzz", { noremap = true })
+vim.keymap.set("n", "N", "Nzz", { noremap = true })
