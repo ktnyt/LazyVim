943edd536ecdc325a40f305a03b4683a90cf98a7
ktnyt <mirei.itaya@lapras.com>
2024-08-25 13:44:39 +0900

    Add empty line insert and better escape keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<cr>", "o<C-[>")
+vim.keymap.set("n", "<S-cr>", "<S-o><C-[>")
+
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 
