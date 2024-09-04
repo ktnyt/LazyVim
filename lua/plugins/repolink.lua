00815b48381f14b232c32eb1a3c0175b13935867
ktnyt <mirei.itaya@lapras.com>
2024-09-04 09:46:57 +0900

    Add repolink plugin

---
+++ lua/plugins/repolink.lua
@@ -0,0 +1,14 @@
+return {
+  "9seconds/repolink.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  cmd = {
+    "RepoLink",
+  },
+
+  opts = {
+    -- your configuration goes here.
+    -- keep empty object if you are fine with defaults
+  },
+}
