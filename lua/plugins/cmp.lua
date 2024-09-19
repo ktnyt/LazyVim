96326c1f9d21dac06eae1d186043d41b1a3e62df
ktnyt <mirei.itaya@lapras.com>
2024-09-19 17:17:25 +0900

    Make formatting work

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,7 +2,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-      cmp = require("cmp")
+      local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping.confirm({ select = true }),
